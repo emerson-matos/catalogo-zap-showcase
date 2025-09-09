6d988a4fa40af1aa900acf1ea0aca02214f0fc0d
Emerson Matos <emegson@live.com>
2025-09-08 21:29:14 -0300

    chore: speed insights?

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,17 +1,20 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
+
 const queryClient = new QueryClient();
 
 export const Providers = ({ children }: { children: React.ReactNode }) => {
-	return (
-		<QueryClientProvider client={queryClient}>
-			<TooltipProvider>
-				{children}
-				<Toaster />
-			</TooltipProvider>
-			<TanStackRouterDevtools />
-		</QueryClientProvider>
-	);
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        {children}
+        <Toaster />
+      </TooltipProvider>
+      <SpeedInsights />
+      <TanStackRouterDevtools />
+    </QueryClientProvider>
+  );
 };
