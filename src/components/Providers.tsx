ab1735c663018d9216d347029b0a203d3572dd22
Emerson Matos <emegson@live.com>
2025-09-08 22:00:58 -0300

    chore: analytics

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,5 +1,6 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/react";
 import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -15,6 +16,7 @@ export const Providers = ({ children }: { children: React.ReactNode }) => {
           {children}
           <Toaster />
         </TooltipProvider>
+        <Analytics />
         <SpeedInsights />
         <TanStackRouterDevtools />
       </QueryClientProvider>
